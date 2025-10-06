5a8e85f697c59fbe2734c8df226a55c515bceeb2
ramkrisheee <ram.krisheee@gmail.com>
2025-10-05 20:39:57 -0400

    commit

--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
+name=input("Enter your name: ")
+print("Hello," , name , "! Welcome to the program.")
+print("Hello," + name + "! Welcome to the program.")
+
 a=100
-for i in range(1,6):
+for i in range(1,1):
     a=i+a
     print(a)
+
+#function to add two numbers
+def add_numbers(x, y):
+    return x / y        
+print(add_numbers(5, 3))
